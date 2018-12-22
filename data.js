27997b744b9afc90b64e501f7ab78247e469aa9e
shathwar <sumanth.hathwar@gmail.com>
2018-12-22 10:27:48 +0000

    Polish the npx business card

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'Sumanth Hathwar',
     title: 'Fullstack developer',
+    email: 'cryptokni8@gmail.com',
     github: 'cryptokni8',
     urls: [
         'https://www.linkedin.com/in/sumanth-hathwar/',
