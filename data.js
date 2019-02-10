233ad9ae41d6220bf2afb4b61b1ad441c0e1a08a
shathwar <sumanth.hathwar@gmail.com>
2019-02-10 19:20:48 +0000

    format data and add gitignore

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 module.exports = {
-    name: 'Sumanth Hathwar',
-    title: 'Fullstack developer',
-    email: 'cryptokni8@gmail.com',
-    github: 'cryptokni8',
-    urls: [
-        'https://www.linkedin.com/in/sumanth-hathwar/',
-        'https://github.com/cryptokni8/'
-    ]
-}
+  name: "Sumanth Hathwar",
+  title: "Fullstack developer",
+  email: "cryptokni8@gmail.com",
+  github: "cryptokni8",
+  urls: [
+    "https://www.linkedin.com/in/sumanth-hathwar/",
+    "https://github.com/cryptokni8/"
+  ]
+};
